893dc501f588b04d85a44b896a03b44d6e4bcb1b
ta2min <ta2mi.prostudy@gmail.com>
2019-11-06 20:19:33 +0900

    add api documets

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,8 @@
+from django.conf.urls import url
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from .views import (
     KakeiboListCreateView,
     KakeiboRetrieveUpdateDestroyView,
@@ -6,7 +10,19 @@ from .views import (
     CategoryRetrieveUpdateDestroyView,
 )
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title='Kakeibo API',
+      default_version='v1',
+      description='家計簿アプリのAPIドキュメント',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    url(r'^documents/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('kakeibo/', KakeiboListCreateView.as_view()),
     path('kakeibo/<pk>/', KakeiboRetrieveUpdateDestroyView.as_view()),
     path('category/', CategoryListCreateView.as_view()),
